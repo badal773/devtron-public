0ee68f1b23d036e6094f668d3621e32f5f946669
Badal Kumar <130441461+badal773@users.noreply.github.com>
2023-11-23 11:09:11 +0530

    Update terraform.sql

--- terraform.sql
+++ terraform.sql
@@ -1,44 +1,43 @@
 INSERT INTO plugin_metadata (id,name,description,type,icon,deleted,created_on,created_by,updated_on,updated_by)
-VALUES (nextval('id_seq_plugin_metadata'),'Terraform Cli','The Terraform CLI, is integrated with Devtron, empowers users to effortlessly execute Terraform scripts, streamlining and enhancing the efficiency of infrastructure provisioning and management.','PRESET','null',false,'now()',1,'now()',1);
-
-INSERT INTO plugin_step_variable (id,plugin_step_id,name,format,description,is_exposed,allow_empty_value,default_value,value,variable_type,value_type,previous_step_index,variable_step_index,variable_step_index_in_plugin,reference_variable_name,deleted,created_on,created_by,updated_on,updated_by) 
-VALUES
-(nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Terraform Cli' and ps."index"=1 and ps.deleted=false),'ProvideImage','STRING','If you want to provide an image','t','t',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1),
-(nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Terraform Cli' and ps."index"=1 and ps.deleted=false),'SelectedOperation','STRING','Please choose the operation (validate, plan, apply, or destroy)','t','f',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1),
-(nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Terraform Cli' and ps."index"=1 and ps.deleted=false),'TfVarsPath','STRING','Please enter the path of the tf.vars (optional)','t','t',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1),
-(nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Terraform Cli' and ps."index"=1 and ps.deleted=false),'ExtraArgs','STRING','Specify additional arguments to include during Terraform apply or plan','t','t',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1);
-(nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Terraform Cli' and ps."index"=1 and ps.deleted=false),'TerraformOutput','STRING','To store the output of Terraform apply or plan command','t','t',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1);
+VALUES (nextval('id_seq_plugin_metadata'),'Terraform Cli','The Terraform CLI, is integrated with Devtron, empowers users to effortlessly execute Terraform scripts, streamlining and enhancing the efficiency of infrastructure provisioning and management.','PRESET','https://raw.githubusercontent.com/badal773/devtron-public/main/file-type-terraform.svg',false,'now()',1,'now()',1);
 
 
 INSERT INTO "plugin_pipeline_script" ("id", "script","type","deleted","created_on", "created_by", "updated_on", "updated_by")
-VALUES (nextval('id_seq_plugin_pipeline_script'),E'#!/bin/bash
-DEFAULT_TF_IMAGE=docker.io/hashicorp/terraform:latest
-if [-z  $ProvideImage ]; then 
-    echo "Using $ProvideImage this image further"
-    DEFAULT_TF_IMAGE=$ProvideImage
-else 
-    echo "Using the default Image --> $DEFAULT_TF_IMAGE"
+VALUES (nextval('id_seq_plugin_pipeline_script'),E'
+export DEFAULT_TF_IMAGE=docker.io/hashicorp/terraform:latest
+if [ -n "$IMAGE" ]; then
+    echo "Using $IMAGE as the custom image."
+    DEFAULT_TF_IMAGE="$IMAGE"
+else
+    echo "Using the default image --> $DEFAULT_TF_IMAGE"
 fi
-echo "Starting this operation $SelectedOperation"
+echo "======================================"
+echo "RUNNING THIS COMMAND "
+echo "docker run -v $PWD:$PWD -w $WORKINGDIR/ --env-file terraform_env_variables.env $DEFAULT_TF_IMAGE $ARGS"
+echo "======================================"
 #exporting all the env variables 
-env > terraform_env_variables.env 
+echo "$ADDITIONALPARAMS"  > terraform_env_variables.env 
+echo "===========secrets==============="
+cat terraform_env_variables.env 
+echo "======================================"
 #RUNNING Terraform init 
-docker run -it  -v $PWD:$PWD -w $PWD/ $DEFAULT_TF_IMAGE init
-
-if [$SelectedOperation == "validate" ] ; then
-#RUNNING Terraform validate
-    docker run -it  -v $PWD:$PWD -w $PWD/ --env-file terraform_env_variables.env $DEFAULT_TF_IMAGE validate 
-
-elif [$SelectedOperation == "plan" ] ; then
-    docker run -it  -v $PWD:$PWD -w $PWD/ --env-file terraform_env_variables.env $DEFAULT_TF_IMAGE plan $ExtraArgs
-elif [$SelectedOperation == "apply" ] ; then
-    docker run -it  -v $PWD:$PWD -w $PWD/ --env-file terraform_env_variables.env $DEFAULT_TF_IMAGE apply -auto-approve  $ExtraArgs
-elif [$SelectedOperation == "destroy" ] ; then
-    docker run -it  -v $PWD:$PWD -w $PWD/ --env-file terraform_env_variables.env $DEFAULT_TF_IMAGE destroy -auto-approve $ExtraArgs
-fi','SHELL','f','now()',1,'now()',1);
+docker run -v $PWD:$PWD -w $PWD/$WORKINGDIR/ $DEFAULT_TF_IMAGE init
+docker run -v $PWD:$PWD -w $PWD/$WORKINGDIR/ --env-file terraform_env_variables.env $DEFAULT_TF_IMAGE $ARGS','SHELL','f','now()',1,'now()',1);
 
 INSERT INTO "plugin_step" ("id", "plugin_id","name","description","index","step_type","script_id","deleted", "created_on", "created_by", "updated_on", "updated_by")
 VALUES (nextval('id_seq_plugin_step'), (SELECT id FROM plugin_metadata WHERE name='Terraform Cli'),'Step 1','Step 1 - Terraform Cli','1','INLINE',(SELECT last_value FROM id_seq_plugin_pipeline_script),'f','now()', 1, 'now()', 1);
+
+INSERT INTO plugin_step_variable (id,plugin_step_id,name,format,description,is_exposed,allow_empty_value,default_value,value,variable_type,value_type,previous_step_index,variable_step_index,variable_step_index_in_plugin,reference_variable_name,deleted,created_on,created_by,updated_on,updated_by) 
+VALUES
+(nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Terraform Cli' and ps."index"=1 and ps.deleted=false),'HTTP_PROXY','STRING','HTTP proxy server for non-SSL requests','t','t',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1),
+(nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Terraform Cli' and ps."index"=1 and ps.deleted=false),'HTTPS_PROXY','STRING','HTTPS proxy server for SSL requests','t','t',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1),
+(nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Terraform Cli' and ps."index"=1 and ps.deleted=false),'NO_PROXY','STRING','no proxy - opt out of proxying HTTP/HTTPS requests','t','t',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1),
+(nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Terraform Cli' and ps."index"=1 and ps.deleted=false),'IMAGE','STRING','The terraform image to use','t','t','docker.io/hashicorp/terraform:latest',null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1),
+(nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Terraform Cli' and ps."index"=1 and ps.deleted=false),'WORKINGDIR','STRING','Source directory','t','t',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1),
+(nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Terraform Cli' and ps."index"=1 and ps.deleted=false),'ARGS','STRING','The terraform cli commands to tun','t','t','--help',null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1),
+(nextval('id_seq_plugin_step_variable'),(SELECT ps.id FROM plugin_metadata p inner JOIN plugin_step ps on ps.plugin_id=p.id WHERE p.name='Terraform Cli' and ps."index"=1 and ps.deleted=false),'ADDITIONALPARAMS','STRING','Provide key value pairs that will be inject in terraform container','t','t',null,null,'INPUT','NEW',null,1,null,null,'f','now()',1,'now()',1);
+
+
 
 
 INSERT INTO plugin_stage_mapping (id,plugin_id,stage_type,created_on,created_by,updated_on,updated_by)
